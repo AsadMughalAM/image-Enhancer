cd329bc97d1968f92ba2a8f342ab94e4ed777e3b
asadmughalam <am127955@gmail.com>
2025-06-04 20:45:34 +0500

    updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => (
   <footer className="w-full bg-white shadow-inner z-50 text-center py-4 px-8 mt-8">
     <span>
       © {new Date().getFullYear()} Image Enhancer. Built by
-      <a href="https://github.com/asad-ali-dev" target="_blank" rel="noopener noreferrer" className="ml-1 font-semibold hover:underline">@Asad Ali</a>
+      <a href=""  rel="" className="ml-1 font-semibold hover:underline">@Asad Ali</a>
     </span>
   </footer>
 );
